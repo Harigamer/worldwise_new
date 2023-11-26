25e9cc16d3a2597226e0b97d599eceada6597068
Harikrishnan B A <harikrishnanasok@gmail.com>
2023-11-26 20:48:33 +0530

    something happened

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,15 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from './components/City';
 import Form from './components/Form';
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 const BASE_URL = "http://localhost:8000";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(BASE_URL + "/cities");
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was an error loading cities data");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
+  
 
   return (
+    <CitiesProvider>
     <BrowserRouter>
       <Routes>
         <Route index element={<HomePage></HomePage>}></Route>
@@ -42,14 +27,15 @@ function App() {
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/app" element={<AppLayout></AppLayout>}>
           <Route index element={<Navigate replace to="cities"></Navigate>}></Route>
-          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>}></Route>
+          <Route path="cities" element={<CityList />}></Route>
           <Route path="cities/:id" element={<City></City>}></Route>
-          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>}></Route>
+          <Route path="countries" element={<CountryList />}></Route>
           <Route path="form" element={<Form></Form>}></Route>
         </Route>
         <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
       </Routes>
     </BrowserRouter>
+    </CitiesProvider>
   );
 }
 
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,15 +2,18 @@ import styles from './CityList.module.css';
 import Spinner from './Spinner';
 import CityItem from './CityItem';
 import Message from './Message';
+import { useCities } from '../contexts/CitiesContext';
 
 
-function CityList({cities,isLoading}) {
+function CityList() {
+    const {cities,isLoading}=useCities();
+
     if(isLoading)
         return <Spinner/>;
     if(!cities.length)
         return <Message message="Add your first city by clicking on a city on the map"/>;
     return (
-        <div className={styles.cityList}>
+        <div className={`${styles.cityList}`}>
             {cities.map((city)=><CityItem city={city} key={city.id}/>)}
         </div>
     )
